6473f558f45bb04fcd9c6091831da2140c35f168
SnakePC <darlitch@al-nsu.ru>
2023-10-27 16:38:34 +0700

    work done

--- ControlWork/Base.hpp
+++ ControlWork/Base.hpp
@@ -1,7 +1,12 @@
 #ifndef BASE_HPP_
 #define BASE_HPP_
 
+#include <stdlib.h>
+#include <time.h>
+
 #include <iostream>
+#include <iterator>
+#include <list>
 #include <map>
 
 class Base {
@@ -9,15 +14,71 @@ class Base {
     char getCondition() {
         return condition;
     }
-    virtual void change() = 0;
+    virtual void change(std::list<Base*>& sequence, std::list<Base*>::iterator& it) {
+        srand(time(NULL));
+        int rand = 1 + std::rand() % 2;
+        // int rand = 1;п
+        switch (rand) {
+        case 1:
+            Rule1(sequence, it);
+            break;
+        case 2:
+            Rule2(sequence, it);
+            break;
+        default:
+            break;
+        }
+    }
+    virtual void Rule1(std::list<Base*>& sequence, std::list<Base*>::iterator& it) = 0;
+    virtual void Rule2(std::list<Base*>& sequence, std::list<Base*>::iterator& it) = 0;
 
    protected:
-    static std::map<char, Base *(*)()> rules;
     char condition;
 };
 
-class T : Base {
-    
+class T : public Base {
+   public:
+    T(char cond) {
+        condition = cond;
+    }
+    void change(std::list<Base*>& sequence, std::list<Base*>::iterator& it) {}
+    void Rule1(std::list<Base*>& sequence, std::list<Base*>::iterator& it) {}
+    void Rule2(std::list<Base*>& sequence, std::list<Base*>::iterator& it) {}
 };
 
-#endif
+class A : public Base {
+   public:
+    A() {
+        condition = 'A';
+    }
+    void Rule1(std::list<Base*>& sequence, std::list<Base*>::iterator& it) {
+        sequence.insert(it, new T('a'));
+        sequence.insert(it, new T('b'));
+        sequence.insert(next(it, 1), new T('b'));
+        sequence.insert(next(it, 2), new T('a'));
+        // it = next(it, 2);
+    }
+    void Rule2(std::list<Base*>& sequence, std::list<Base*>::iterator& it) {
+        sequence.insert(next(it, 1), new T(' '));
+        delete *it;
+        it = sequence.erase(it);
+    }
+};
+
+class B : public Base {
+   public:
+    B() {
+        condition = 'B';
+    }
+    void Rule1(std::list<Base*>& sequence, std::list<Base*>::iterator& it) {
+        sequence.insert(next(it, 1), new A);
+        // it = next(it, 1);
+    }
+    void Rule2(std::list<Base*>& sequence, std::list<Base*>::iterator& it) {
+        sequence.insert(next(it, 1), new T('.'));
+        delete *it;
+        it = sequence.erase(it);
+    }
+};
+
+#endif  // BASE_HPP_
--- ControlWork/main.cpp
+++ ControlWork/main.cpp
@@ -1,8 +1,20 @@
-#include <list>
-
 #include "Base.hpp"
 
 int main() {
     std::list<Base*> sequence;
-    
+    // sequence.push_back(new A);
+    sequence.push_back(new B);
+    for (std::size_t i = 0; i < 20; ++i) {
+        for (std::list<Base*>::iterator it = sequence.begin(); it != sequence.end(); ++it) {
+            (*it)->change(sequence, it);
+        }
+        for (auto const& el : sequence) {
+            std::cout << el->getCondition();
+        }
+        std::cout << std::endl;
+    }
+    for (auto const& el : sequence) {
+        delete el;
+    }
+    sequence.clear();
 }
