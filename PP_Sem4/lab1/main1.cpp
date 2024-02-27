e4f5307b946f143f90dfb1563f6d7c18d92add96
SnakePC <darlitch@al-nsu.ru>
2024-02-27 12:25:43 +0700

    var.1 just work

--- PP_Sem4/lab1/main1.cpp
+++ PP_Sem4/lab1/main1.cpp
@@ -8,9 +8,8 @@
 
 using matrix_cont = std::vector<std::vector<double>>;
 
-const std::size_t N = 17;
-const double e = 0.00001;
-const double t = 0.01;
+const std::size_t N = 6800;
+const double e = 0.000001;
 
 std::size_t FindLrows(int size, int rank) {
     return (N / size + (N % size > rank));
@@ -72,14 +71,14 @@ void AMultX(matrix_cont& matrix, double* x, double* b) {
     int* lrowsV = FindLrowsV(size);
     int* beginV = FindBeginV(size);
     double b1[lrows];
-    std::cout << "lrows: " << lrows << std::endl;
+    // std::cout << "lrows: " << lrows << std::endl;
     for (std::size_t i = 0; i < lrows; ++i) {
         b1[i] = b[i + matrixBegin];
     }
-    for (std::size_t i = 0; i < lrows; ++i) {
-        std::cout << b1[i] << " ";
-    }
-    std::cout << std::endl;
+    // for (std::size_t i = 0; i < lrows; ++i) {
+    //     std::cout << b1[i] << " ";
+    // }
+    // std::cout << std::endl;
     for (std::size_t i = 0; i < lrows; ++i) {
         for (std::size_t j = matrixBegin; j < matrixBegin + lrows; ++j) {
             b1[i] += x[i + matrixBegin] * matrix[i][j];
@@ -96,7 +95,7 @@ void SubB(double* x1, double* b) {
     }
 }
 
-void MultT(double* x1) {
+void MultT(double* x1, double t) {
     for (std::size_t i = 0; i < N; ++i) {
         x1[i] *= t;
     }
@@ -120,6 +119,8 @@ void SearchX(matrix_cont& matrix, double* x, double* b) {
     double x0[N];
     double x1[N];
     double u;
+    double uOld;
+    double t = 0.01;
     int size, rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -137,19 +138,30 @@ void SearchX(matrix_cont& matrix, double* x, double* b) {
         //     std::cout << x1[i] << " - ";
         // }
         // std::cout << std::endl;
-        MultT(x1);
+        MultT(x1, t);
         SubXX(x0, x1);
         AMultX(matrix, x0, x1);
         SubB(x1, b);
         u = Module(x1);
         // std::cout << u << std::endl;
         u = u / Module(b);
+        if (u - uOld > 100) {
+            t = -t;
+        }
+        uOld = u;
         // std::cout << u << std::endl;
     } while (u > e);
+    if (rank == 0) {
+        for (std::size_t i = 0; i < 10; ++i) {
+            std::cout << x1[i] << " ";
+        }
+        std::cout << std::endl;
+    }
     std::cout << rank << " Vector found" << std::endl;
 }
 
 int main(int argc, char** argv) {
+    double startTime, endTime;
     int rank, size;
     double x[N];
     double b[N] = {0};
@@ -158,19 +170,26 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     matrix_cont matrix = MatrixBuilder(size, rank);
     if (rank == 0) {
+        startTime = MPI_Wtime();
         RandomVectorX(x);
     }
     MPI_Bcast(x, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    for (std::size_t i = 0; i < N; ++i) {
-        std::cout << x[i] << " ";
+    if (rank == 0) {
+        for (std::size_t i = 0; i < 10; ++i) {
+            std::cout << x[i] << " ";
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
     AMultX(matrix, x, b);
-    for (std::size_t i = 0; i < N; ++i) {
-        std::cout << b[i] << " ";
-    }
-    std::cout << std::endl;
+    // for (std::size_t i = 0; i < N; ++i) {
+    //     std::cout << b[i] << " ";
+    // }
+    // std::cout << std::endl;
     SearchX(matrix, x, b);
+    if (rank == 0) {
+        endTime = MPI_Wtime();
+        std::cout << "Time: " << endTime - startTime << std::endl;
+    }
     MPI_Finalize();
     return 0;
 }
