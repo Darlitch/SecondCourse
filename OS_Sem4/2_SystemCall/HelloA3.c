ed2239a7dad3280edaea30a3b34fc802779eb3f6
SnakePC <darlitch@al-nsu.ru>
2024-03-08 23:41:56 +0700

    08.03.2024

--- OS_Sem4/2_SystemCall/HelloA3.c
+++ OS_Sem4/2_SystemCall/HelloA3.c
@@ -3,13 +3,15 @@
 
 #include "stdio.h"
 
-static ssize_t mywrite (int fd, const void *buf, size_t count)
-{
+static ssize_t mywrite(int fd, const void *buf, size_t count) {
     return syscall(SYS_write, fd, buf, count);
 }
 
 int main() {
     // printf("Hello world");
-    mywrite(1, "Hello world!\n", 13);
+    ssize_t err = write(1, "Hello world!\n", 13);
+    if (err == -1 || err == NULL) {
+        return 1;
+    }
     return 0;
 }
