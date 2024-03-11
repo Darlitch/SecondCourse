e85dd2ceecf5af8b5c498dda4f115396f27397be
SnakePC <darlitch@al-nsu.ru>
2024-03-11 13:47:16 +0700

    update gitignore

---
+++ OOP_Sem4/CalcParser/Ver2/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String formula = "(2+2)*2";
+        MathParser mp = new MathParser();
+        try {
+            System.out.println("= " + mp.Parse(formula));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
