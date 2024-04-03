66de8f6241eed67dcdec750c6205a04007c4f9f0
SnakePC <darlitch@al-nsu.ru>
2024-04-04 01:48:19 +0700

    03.04.2024

--- PP_Sem4/lab3/copy.cpp
+++ PP_Sem4/lab3/copy.cpp
@@ -1,281 +1,226 @@
-#include <iostream>
+#include <malloc.h>
+#include <math.h>
 #include <mpi.h>
-#include <algorithm>
-#include <numeric>
-#include <array>
+#include <stdio.h>
 
-    struct TSizes {
-        TSizes(int p1, int p2, int N1, int M1, int N2, int M2) {
-        sizeA = new int[p1];
-        sizeB = new int[p2];
-        displsA = new int[p1];
-        displsB = new int[p2];
+#include <ctime>
+#include <iostream>
 
-        //size every block
-        std::fill(sizeA, sizeA+p1, N1/p1 * M1);
-        for(int ost = N1%p1, i = 0; ost != 0; --ost, ++i) {
-            sizeA[i] += M1;
-        }
+int N1 = 4;
+int N2 = 4;
+int N3 = 4;
 
-        std::fill(sizeB, sizeB+p2, M2/p2 * N2);
-        for(int ost = M2%p2, i = 0; ost != 0; --ost, ++i) {
-            sizeB[i] += N2;
-        }
-
-        //displ every block
-
-        displsA[0] = 0;
-        for (int i = 1; i < p1; ++i) {
-            displsA[i] = displsA[i-1] + sizeA[i-1];
-        }
-
-        displsB[0] = 0;
-        for (int i = 1; i < p2; ++i) {
-            displsB[i] = displsB[i-1] + sizeB[i-1];
-        }
+int* Recvcounts_for_matrixA(int N, int size) {
+    int* array = new int[size];
+    for (int j = 0; j < size; j++) {
+        array[j] = ((N / size) + (int)(N % size > j)) * N2;
     }
-    int *sizeA;
-    int *sizeB;
-    int *displsA;
-    int *displsB;
-};
-
-void check(int argc, char** argv, int* ret) {
-    int countRank;
-    MPI_Comm_size(MPI_COMM_WORLD, &countRank);
-    if (argc != 7) throw std::invalid_argument("count argv != 4");
-    try {
-        ret[0] = std::stoi(argv[1]); //p1
-        ret[1] = std::stoi(argv[2]); //p2
-        ret[2] = std::stoi(argv[3]); //N1
-        ret[3] = std::stoi(argv[4]); //M1
-        ret[4] = std::stoi(argv[5]); //N2
-        ret[5] = std::stoi(argv[6]); //M2
-    } catch (std::invalid_argument ex) {
-        throw std::invalid_argument("input parametr is not number");
-    } 
-    if (ret[0] * ret[1] != countRank) {
-        throw std::invalid_argument ("countP1 * count P2 != MPI_COMM_WORLD size");
-    }
-    if (ret[4] != ret[3] || ret[2] <= 0 ||
-    ret[3] <= 0 || ret[4] <= 0 || ret[5] <= 0 ) {
-        throw std::invalid_argument("wrong dim matrix");
-    }
+    return array;
 }
 
-void transposeMatrix(double* matrix, int N, int M) {
-    double* transposed = new double[M * N];
-
-    for (int i = 0; i < N; ++ i) {
-    for (int j = 0; j < M; ++j) {
-    transposed[j * N + i] = matrix[i * M + j];
+int OffSet(int size, int rank, int N) {
+    int count = 0;
+    for (int i = 0; i < rank; i++) {
+        count += (N / size) + (int)(N % size > i);
     }
-    }
-    std::copy(transposed, transposed + N*M, matrix);
-    delete[] transposed;
+    return count;
 }
 
-void fillMatrix(double* matrix, int N, int M) {
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M; ++j) {
-            matrix[i * M + j] = rand() % 100;
-            if (rand() % 2) {
-                matrix[i * M + j]*= -1;
+int* Displs_for_matrixA(int N, int size) {
+    int* array = new int[size];
+    for (int j = 0; j < size; j++) {
+        array[j] = OffSet(size, j, N) * N2;
+    }
+    return array;
+}
+
+double* Build_matrix_C(double* part_C, int ABlockSize, int BBlockSize, int rank, int* processesOnYandX) {
+    double* C = nullptr;
+    MPI_Datatype block, blocktype;
+    MPI_Type_vector(ABlockSize, BBlockSize, N3, MPI_DOUBLE, &block);
+    MPI_Type_commit(&block);
+
+    MPI_Type_create_resized(block, 0, BBlockSize * sizeof(double), &blocktype);
+    MPI_Type_commit(&blocktype);
+
+    int* displ = new int[processesOnYandX[0] * processesOnYandX[1]]{0};
+    int* rcount = new int[processesOnYandX[0] * processesOnYandX[1]]{0};
+    int BlockCount = 0;
+    int BlockSize = ABlockSize * BBlockSize;
+
+    for (int i = 0; i < processesOnYandX[0]; i++) {
+        for (int j = 0; j < processesOnYandX[1]; j++) {
+            rcount[i * processesOnYandX[1] + j] = 1;
+            displ[i * processesOnYandX[1] + j] = BlockCount;
+            BlockCount += 1;
+        }
+        BlockCount += ((N1 / processesOnYandX[0]) + (int)(N1 % processesOnYandX[0] > i) - 1) * processesOnYandX[1];
+    }
+
+    if (rank == 0) {
+        C = new double[N1 * N3];
+    }
+
+    MPI_Gatherv(part_C, BlockSize, MPI_DOUBLE, C,
+                rcount, displ, blocktype, 0, MPI_COMM_WORLD);
+    delete[] displ;
+    delete[] rcount;
+    return C;
+}
+
+void New_MPI_Comm(int* processesOnXandY, int rank, int size, MPI_Comm* comm_cart, MPI_Comm* comm_rows, MPI_Comm* comm_colm) {
+    const int period[2] = {false, false};
+    MPI_Cart_create(MPI_COMM_WORLD, 2, processesOnXandY, period, true, comm_cart);
+    int* remain_dims = new int[2];
+    remain_dims[1] = true;
+    remain_dims[0] = false;
+    MPI_Cart_sub(*comm_cart, remain_dims, comm_colm);
+    remain_dims[1] = false;
+    remain_dims[0] = true;
+    MPI_Cart_sub(*comm_cart, remain_dims, comm_rows);
+}
+
+void Multiplication(double* part_A, int lenPart_A, double* part_B, int lenPart_B, double* part_C) {
+    for (int i = 0; i < lenPart_A; i++) {
+        for (int k = 0; k < N2; k++) {
+            for (int j = 0; j < lenPart_B; j++) {
+                part_C[i * lenPart_B + j] += part_A[i * N2 + k] * part_B[k * lenPart_B + j];
             }
         }
     }
 }
 
-int main(int argc, char** argv) { // argv: p1, p2, N1, M1, N2, M2
+void Check(int rank, double* A, double* B, double* C) {
+    if (rank == 0) {
+        double* proof_C = new double[N3 * N1]{0};
+        for (int i = 0; i < N1; i++) {
+            for (int j = 0; j < N3; j++) {
+                for (int k = 0; k < N2; k++) {
+                    proof_C[i * N3 + j] += A[i * N2 + k] * B[k * N3 + j];
+                }
+            }
+        }
+        bool corect = true;
+        for (int i = 0; i < N3; i++) {
+            for (int j = 0; j < N1; j++) {
+                if (C[i * N3 + j] - proof_C[i * N3 + j] != 0) {
+                    corect = false;
+                }
+            }
+        }
+        printf("corect:(%d)\n", (int)corect);
+        delete[] proof_C;
+    }
+}
+
+void Create_Matrix_Part_A(double* part_A, double* A, int* processesOnYandX, int lenPart_A, MPI_Comm* comm_rows) {
+    int* recvcounts_A = Recvcounts_for_matrixA(N1, processesOnYandX[0]);
+    int* displs_A = Displs_for_matrixA(N1, processesOnYandX[0]);
+
+    MPI_Scatterv(A, recvcounts_A, displs_A, MPI_DOUBLE, part_A, lenPart_A * N2, MPI_DOUBLE, 0, *comm_rows);
+
+    delete[] recvcounts_A;
+    delete[] displs_A;
+}
+
+void Create_Matrix_Part_B(double* part_B, double* B, int* processesOnYandX, int lenPart_B, MPI_Comm* comm_colms) {
+    MPI_Datatype col, coltype;
+
+    MPI_Type_vector(N2, lenPart_B, N3, MPI_DOUBLE, &col);
+    MPI_Type_commit(&col);
+    MPI_Type_create_resized(col, 0, (lenPart_B) * sizeof(double), &coltype);
+    MPI_Type_commit(&coltype);
+
+    MPI_Scatter(B, 1, coltype, part_B, lenPart_B * N2, MPI_DOUBLE, 0, *comm_colms);
+}
+
+int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
-    srand(21212);
-    int myRank, countRank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int *inp = new int[6];
+    MPI_Comm* comm_cart = new MPI_Comm;
+    MPI_Comm* comm_rows = new MPI_Comm;
+    MPI_Comm* comm_colms = new MPI_Comm;
 
-    //check input
-    bool error = false;
-    if(!myRank) {
-    try {
-    check(argc, argv, inp);
-    } catch (std::invalid_argument ex) {
-    std::cout << ex.what() << std::endl;
-    error = true;
-    }
-    }
-    MPI_Bcast(&error, 1, MPI_CXX_BOOL, 0, MPI_COMM_WORLD);
-    if (error) return 1; 
+    double* A = nullptr;
+    double* B = nullptr;
 
-    // send input args
-    MPI_Bcast(inp, 6, MPI_INT, 0, MPI_COMM_WORLD);
-    //calc sizes
-    TSizes sizes = TSizes(inp[0], inp[1], inp[2], inp[3], inp[4], inp[5]); 
-    //create virtual matrix of threads 
-    MPI_Comm vMatrix;
-    int dimsMatrix[2] = {inp[0], inp[1]};
-    int periods[2] = {0, 0};
-    MPI_Cart_create(MPI_COMM_WORLD, 2, dimsMatrix, periods, 0, &vMatrix);
-    int coords[2];
-    MPI_Cart_coords(vMatrix, myRank, 2, coords);
-
-    //std::cout << myRank << " " << coords[0] << " " << coords[1] << std::endl;
-    //send
-
-    double *AMatrix;
-    if (!myRank) std::cout << "sendA" << std::endl;
-    //send A matrix
-    { 
-    double* AMatrixTMP;
-    if (!myRank) {
-    AMatrixTMP = new double[inp[2] * inp[3]];
-    fillMatrix(AMatrixTMP, inp[2], inp[3]);
-
-    /*std::cout << "Matrix A: " << std::endl;
-    for (int i = 0; i < inp[2]; ++i) {
-    std::for_each(AMatrixTMP + i*inp[3], AMatrixTMP + i*inp[3] + inp[3], [](double &n){
-    std::cout << n << " ";
-    });
-    std::cout << std::endl; 
-    }*/ 
-    } 
-
-    int color = (coords[1] == 0) ? 0 : MPI_UNDEFINED;
-    int key = coords[0];
-    AMatrix = new double[sizes.sizeA[key]];
-    MPI_Comm lineComm = MPI_COMM_NULL;
-    MPI_Comm_split(MPI_COMM_WORLD, color, key, &lineComm);
-    if (lineComm != MPI_COMM_NULL) {
-    if (coords[0] == 0) {
-    std::copy(AMatrixTMP, AMatrixTMP + sizes.sizeA[key], AMatrix);
-    }
-    MPI_Scatterv(AMatrixTMP, sizes.sizeA, sizes.displsA,
-    MPI_DOUBLE, AMatrix,
-    sizes.sizeA[key], MPI_DOUBLE,
-    0, lineComm);
-    MPI_Comm_free(&lineComm);
+    // if (rank == 0) {
+    B = new double[N3 * N2];
+    for (size_t i = 0; i < N3; i++) {
+        for (size_t j = 0; j < N2; j++) {
+            B[i * N2 + j] = rand() % 10;
+        }
     }
 
-    if (!myRank) delete[] AMatrixTMP; 
+    A = new double[N1 * N2];
+    for (size_t i = 0; i < N1; i++) {
+        for (size_t j = 0; j < N2; j++) {
+            A[i * N2 + j] = rand() % 10;
+        }
     }
+    //}
 
-    double *BMatrix = new double[sizes.sizeB[0]];
-    //send B matrix
-    if (!myRank) std::cout << "sendB" << std::endl;
-    {
-    double* BMatrixTMP = nullptr;
-    if (!myRank) {
-    BMatrixTMP = new double[inp[4] * inp[5]];
-    std::cout << std::endl;
-    fillMatrix(BMatrixTMP, inp[4], inp[5]);
-    /* std::cout << "Matrix B: " << std::endl;
-    for (int i = 0; i < inp[4]; ++i) {
-    std::for_each(BMatrixTMP + i*inp[5], BMatrixTMP + i*inp[5] + inp[5], [](double &n){
-    std::cout << n << " ";
-    });
-    std::cout << std::endl; 
-    } */
-    transposeMatrix(BMatrixTMP, inp[4], inp[5]);
-    }
-    int color = (coords[0] == 0) ? 0 : MPI_UNDEFINED;
-    int key = coords[1];
-    MPI_Comm lineComm = MPI_COMM_NULL;
-    MPI_Comm_split(MPI_COMM_WORLD, color, key, &lineComm);
-    if (lineComm != MPI_COMM_NULL) {
-    if (coords[1] == 0) {
-    std::copy(BMatrixTMP, BMatrixTMP + sizes.sizeB[key], BMatrix);
-    }
-    MPI_Scatterv(BMatrixTMP, sizes.sizeB, sizes.displsB,
-    MPI_DOUBLE, BMatrix,
-    sizes.sizeB[key], MPI_DOUBLE,
-    0, lineComm);
-    MPI_Comm_free(&lineComm);
-    }
-    if (!myRank) delete[] BMatrixTMP;
-    }
+    int* processesOnYandX = new int[2]{0};
 
-    if (!myRank) std::cout << "sendSubA" << std::endl;
-    {
-    int color = coords[0];
-    int key = coords[1];
-    MPI_Comm lineComm = MPI_COMM_NULL;
-    MPI_Comm_split(MPI_COMM_WORLD, color, key, &lineComm);
-    MPI_Bcast(AMatrix, sizes.sizeA[color], MPI_DOUBLE, 0, lineComm);
-    MPI_Comm_free(&lineComm);
-    }
+    MPI_Dims_create(size, 2, processesOnYandX);
 
-    //bcast submatrix B
-    if (!myRank) std::cout << "sendSubB" << std::endl;
-    {
-    int color = coords[1];
-    int key = coords[0];
-    MPI_Comm lineComm = MPI_COMM_NULL;
-    MPI_Comm_split(MPI_COMM_WORLD, color, key, &lineComm);
-    MPI_Bcast(BMatrix, sizes.sizeB[color], MPI_DOUBLE, 0, lineComm);
-    MPI_Comm_free(&lineComm);
-    }
+    New_MPI_Comm(processesOnYandX, rank, size, comm_cart, comm_rows, comm_colms);
 
-    //mult submatrix
-    int localN = sizes.sizeA[coords[0]] / inp[3],
-    localM = sizes.sizeB[coords[1]] / inp[4];
-    double *localResult = new double[localN * localM];
+    // ���������� ������� A
+    int rank_row;
+    MPI_Comm_rank(*comm_rows, &rank_row);
+    int lenPart_A = (N1 / processesOnYandX[0]) + (int)(N1 % processesOnYandX[0] > rank_row);
+    double* part_A = new double[lenPart_A * N2];
+    Create_Matrix_Part_A(part_A, A, processesOnYandX, lenPart_A, comm_rows);
 
-    for (int i = 0; i < localN; ++i) {
-    for (int j = 0; j < localM; ++j) {
-    int posA = i * inp[3],
-    posB = j * inp[3];
-    localResult[i*localM + j] = std::inner_product(AMatrix + posA, AMatrix + posA + inp[3], BMatrix + posB, 0);
-    }
-    }
+    // ���������� ������� B
+    int rank_colm;
+    MPI_Comm_rank(*comm_colms, &rank_colm);
+    int lenPart_B = (N3 / processesOnYandX[1]) + (int)(N3 % processesOnYandX[1] > rank_colm);
+    double* part_B = new double[lenPart_B * N2];
+    Create_Matrix_Part_B(part_B, B, processesOnYandX, lenPart_B, comm_colms);
 
-    //send/recv 
-    if (myRank) {
-    MPI_Send(localResult, localN*localM, MPI_DOUBLE, 0, myRank, MPI_COMM_WORLD);
-    }
-    double* result = nullptr;
-    if (!myRank) {
+    // �������� ������ ������� C
+    double* part_C = new double[lenPart_B * lenPart_A]{0};
+    Multiplication(part_A, lenPart_A, part_B, lenPart_B, part_C);
 
-    result = new double[inp[2] * inp[5]];
-    for (int i = 0; i < inp[0]*inp[1]; ++i) {
+    // �������� ������� C
+    double* C = Build_matrix_C(part_C, lenPart_A, lenPart_B, rank, processesOnYandX);
 
-    int tmpCoords[2];
-    MPI_Cart_coords(vMatrix, i, 2, tmpCoords);
-    int recvN = sizes.sizeA[tmpCoords[0]] / inp[3],
-    recvM = sizes.sizeB[tmpCoords[1]] / inp[4];
-    double* tmpBuf = new double[recvN * recvM];
-    if (i) {
-    MPI_Recv(tmpBuf, recvN * recvM, MPI_DOUBLE, i, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    } else {
-    std::copy(localResult, localResult + recvM * recvN, tmpBuf);
-    }
+    Check(rank, A, B, C);
 
-    int displN = sizes.displsA[tmpCoords[0]] / inp[3],
-    displM = sizes.displsB[tmpCoords[1]] / inp[3];
-    for(int j = 0; j < recvN; ++j) {
-    std::copy(tmpBuf + j * recvM, tmpBuf + j * recvM + recvM, result + displM + (displN + j) * inp[5]);
+    std::cout << "A_Part in process " << rank << "\n";
+    for (int i = 0; i < lenPart_A; i++) {
+        for (int j = 0; j < N2; j++) {
+            std::cout << part_A[i * N2 + j] << " ";
+        }
+        std::cout << "\n";
     }
-    delete[] tmpBuf;
-    }
-    }
+    std::cout << "\n";
 
-    delete[] AMatrix;
-    delete[] BMatrix;
-    delete[] localResult;
-    MPI_Comm_free(&vMatrix);
-
-    // out
-    if (!myRank) {
-    /*std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << std::endl;
-    for (int i = 0; i < inp[2]; ++i) {
-    std::for_each(result + i * inp[5], result + i * inp[5] + inp[5], 
-    [](double &n) { std::cout << n << " "; });
-    std::cout << std::endl;
-    } */
-    delete[] result;
+    std::cout << "B_Part in process " << rank << "\n";
+    for (int i = 0; i < N2; i++) {
+        for (int j = 0; j < lenPart_B; j++) {
+            std::cout << part_B[i * lenPart_B + j] << " ";
+        }
+        std::cout << "\n";
     }
+    std::cout << "\n";
+
+    delete[] A;
+    delete[] B;
+    delete[] C;
+    delete[] part_A;
+    delete[] part_B;
+    delete[] part_C;
+    delete[] processesOnYandX;
+    delete comm_cart;
+    delete comm_rows;
+    delete comm_colms;
 
     MPI_Finalize();
-    return 0;
 }
--- PP_Sem4/lab3/main.cpp
+++ PP_Sem4/lab3/main.cpp
@@ -1,54 +1,106 @@
-#include <iostream>
 #include <mpi.h>
 
-const std::size_t N1 = 4;
-const std::size_t N2 = 4;
-const std::size_t N3 = 8;
+#include <iostream>
+
+const std::size_t N1 = 3;
+const std::size_t N2 = 2;
+const std::size_t N3 = 6;
 
 double* MatrixBuilder(std::size_t n1, std::size_t n2) {
     double* matrix = new double[n1 * n2];
-    std::fill_n(matrix, n1*n2, 1.0);
+    std::fill_n(matrix, n1 * n2, 3.0);
     return matrix;
 }
 
-double* MultMatrix(double* matrixA, double* matrixB, std::size_t Ar, std::size_t Bc) {
-    double* partMatrixC = new double[Ar * Bc];
-    for (std::size_t i = 0; i < Ar; ++i) {
-        for (std::size_t j = 0; j < Bc; ++j) {
-            partMatrixC[i * Bc + j] = 0;
+double* MultMatrix(double* partA, double* partB, std::size_t rowsA, std::size_t colsB) {
+    double* partMatrixC = new double[rowsA * colsB];
+    for (std::size_t i = 0; i < rowsA; ++i) {
+        for (std::size_t j = 0; j < colsB; ++j) {
+            partMatrixC[i * colsB + j] = 0;
             for (std::size_t k = 0; k < N2; ++k) {
-                partMatrixC[i * Bc + j] += matrixA[i * N2 + k] * matrixB[k * N3 + j];
+                partMatrixC[i * colsB + j] += partA[i * N2 + k] * partB[k * N3 + j];
             }
         }
     }
     return partMatrixC;
 }
 
+void CreateComm(int* dims, MPI_Comm* distr_rows_comm, MPI_Comm* distr_cols_comm) {
+    int periods[2] = {0};
+    MPI_Comm* comm_cart = new MPI_Comm;
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, true, comm_cart);
+    int distrDims[2] = {1, 0};
+    MPI_Cart_sub(*comm_cart, distrDims, distr_rows_comm);
+    distrDims[0] = 0;
+    distrDims[1] = 1;
+    MPI_Cart_sub(*comm_cart, distrDims, distr_cols_comm);
+    delete comm_cart;
+}
+
+double* ScatterA(MPI_Comm* distr_comm, double* m, int* dims, int lenPart) {
+}
+
+double* ScatterB(MPI_Comm* distr_comm, double* m, int* dims, int lenPart) {
+}
+
 int main(int argc, char** argv) {
     double startTime, endTime;
+    int* dims = new int[2]{0};
     int rank, size;
     double* matrixA;
     double* matrixB;
     double* matrixC;
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size); 
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     if (rank == 0) {
         matrixA = MatrixBuilder(N1, N2);
         matrixB = MatrixBuilder(N2, N3);
-        matrixC = MultMatrix(matrixA, matrixB, N1, N3);
     }
-    for (std::size_t i = 0; i < N1; ++i) {
-        for (std::size_t j = 0; j < N3; ++j) {
-            std::cout << matrixC[i * N3 + j] << " ";
-        }
-        std::cout << std::endl;
+
+    startTime = MPI_Wtime();
+    MPI_Dims_create(size, 2, dims);
+    MPI_Comm* distr_rows_comm = new MPI_Comm;
+    MPI_Comm* distr_cols_comm = new MPI_Comm;
+    CreateComm(dims, distr_rows_comm, distr_cols_comm);
+
+    int drows_rank, dcols_rank;
+    MPI_Comm_rank(*distr_rows_comm, &drows_rank);
+    int lenPartA = (N1 / dims[0]) + (int)(N1 % dims[0] > drows_rank);
+    double* partA = ScatterA(distr_rows_comm, matrixA, dims, lenPartA);
+    MPI_Comm_rank(*distr_cols_comm, &dcols_rank);
+    int lenPartB = (N3 / dims[1]) + (int)(N3 % dims[1] > dcols_rank);
+    double* partB = ScatterB(distr_cols_comm, matrixB, dims, lenPartB);
+
+    double* partC = MultMatrix(partA, partB, lenPartA, lenPartB);
+
+    // matrixC = MultMatrix(matrixA, matrixB, N1, N3);
+    if (rank == 0) {
+        endTime = MPI_Wtime();
+        std::cout << "Time: " << endTime - startTime << std::endl;
+        delete matrixA;
+        delete matrixB;
     }
+    // for (std::size_t i = 0; i < N1; ++i) {
+    //     for (std::size_t j = 0; j < N3; ++j) {
+    //         std::cout << matrixC[i * N3 + j] << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
     // for (std::size_t i = 0; i < N2; ++i) {
     //     for (std::size_t j = 0; j < N3; ++j) {
     //         std::cout << matrixB[i * N3 + j] << " ";
     //     }
     //     std::cout << std::endl;
     // }
+    // delete[] matrixC;
+    delete distr_rows_comm;
+    delete distr_cols_comm;
+    // delete[] partA;
+    // delete[] partB;
+    // delete[] partC;
+    delete[] dims;
+    MPI_Finalize();
     return 0;
 }
