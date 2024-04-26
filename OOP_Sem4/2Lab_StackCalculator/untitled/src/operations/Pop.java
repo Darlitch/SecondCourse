10336f45293b0bb1496fdfb87a93a2e66e55cb57
Snake <snake@WIN-UN8PRG7DQ3V>
2024-04-26 13:25:45 +0700

    transfer to PC

--- OOP_Sem4/2Lab_StackCalculator/untitled/src/operations/Pop.java
+++ OOP_Sem4/2Lab_StackCalculator/untitled/src/operations/Pop.java
@@ -4,14 +4,12 @@ import Except.CalcExceptions;
 
 import java.util.Stack;
 
-public class Push extends Oper {
+public class Pop extends Oper {
     public double doOper(Object[] args) throws CalcExceptions {
-        if (args.length != 2) {
+        if (args.length != 1) {
             throw new CalcExceptions("Error: invalid count of args");
         }
         Stack<Double> stack = (Stack<Double>)args[0];
-        double val = (double)args[1];
-        stack.push(val);
-        return 0;
+        return stack.pop();
     }
 }
